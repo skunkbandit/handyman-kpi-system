62e406a83c907c9c763b834bc999d488938ece2b
skunkbandit <3980820+skunkbandit@users.noreply.github.com>
2025-03-19 15:48:46 -0500

    Delete src directory

--- src/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-KPI System Package
------------------
-This package contains the KPI system application for tracking
-handyman business employee performance metrics.
-"""
--- src/models/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Database Models Package
-----------------------
-This package contains SQLAlchemy models that represent
-the database schema for the KPI system.
-"""
